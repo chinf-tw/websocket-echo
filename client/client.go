431ee499dea7c1287886606560515926297b5271
CHINF <chinf1996@gmail.com>
2020-11-20 15:21:02 +0800

    初版 websocket echo server and client

---
+++ client/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	c, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:8080/echo", nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	wg.Add(1)
+	defer c.Close()
+	go func(wg *sync.WaitGroup) {
+		_, msg, err := c.ReadMessage()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println(string(msg))
+		wg.Done()
+	}(&wg)
+	c.WriteMessage(websocket.TextMessage, []byte("TEST!!"))
+	wg.Wait()
+}
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var counter = 0
+
+func main() {
+	r := gin.Default()
+	r.GET("/echo", echo)
+	fmt.Println("listen and serve webscoket on 0.0.0.0:8080/echo")
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+func echo(c *gin.Context) {
+	upgrade := &websocket.Upgrader{
+		//如果有 cross domain 的需求，可加入這個，不檢查 cross domain
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+	var wg sync.WaitGroup
+
+	ws, err := upgrade.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		return
+	}
+	defer func() {
+		log.Println("disconnect !!")
+		if err = ws.Close(); err != nil {
+			log.Println("ws.Close()問題\n", err)
+		}
+	}()
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		var msg []byte
+		for {
+			counter++
+			_, msg, err = ws.ReadMessage()
+			fmt.Println(strconv.Itoa(counter)+"client: ", string(msg))
+			if err != nil {
+				log.Println(err)
+				wg.Done()
+				return
+			}
+			msg = []byte(strconv.Itoa(counter) + ". server: " + string(msg))
+			if err := ws.WriteMessage(websocket.TextMessage, msg); err != nil {
+				log.Println(err)
+				wg.Done()
+				return
+			}
+		}
+	}(&wg)
+	wg.Wait()
+}
